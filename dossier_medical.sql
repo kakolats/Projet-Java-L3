ad4d2c7350fc9063a4d64c56555b97c46c76a526
Stephane AKOLATSE <komla.akolatse@ism.edu.sn>
2021-10-24 22:20:04 +0100

    first commit

---
+++ dossier_medical.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : Dim 24 oct. 2021 à 21:14
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet_java_l3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dossier_medical`
+--
+
+DROP TABLE IF EXISTS `dossier_medical`;
+CREATE TABLE IF NOT EXISTS `dossier_medical` (
+  `id` int(11) NOT NULL,
+  `consultations` json NOT NULL,
+  `prestations` json NOT NULL,
+  `id_patient` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
